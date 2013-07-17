8e512c0832b10b1e06efc054b219e333c4db6e74
Rossaye Abdool Kureem <kureem@gmail.com>
2013-07-17 13:13:56 +0400

    some more houskeeping

--- ui/castafioreframework/src/main/java/org/castafiore/easyui/layout/TabModel.java
+++
@@ -1,9 +0,0 @@
-package org.castafiore.easyui.layout;
-
-public interface TabModel extends org.castafiore.ui.ex.tab.TabModel{
-	
-	public boolean isClosable(int index);
-	
-	public String getIconCls(int index);
-
-}
